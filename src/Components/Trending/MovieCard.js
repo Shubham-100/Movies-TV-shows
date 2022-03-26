6c1b6c891b93a5c5b4308e86e6a9328dc0273753
aryan jangid <aryan100jangid@gmail.com>
2022-03-26 14:51:57 +0530

    Add Trending movies and TV shows

---
+++ src/Components/Trending/MovieCard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MovieCard({title, date, vote, image}) {
+  return (
+    <div>{title}</div>
+  )
+}
+
+export default MovieCard
--- src/Components/Trending/Trending.js
+++ src/Components/Trending/Trending.js
@@ -1,34 +1,31 @@
 import React, { useState, useEffect } from 'react'
-import './trending.css';
+import MovieCard from './MovieCard';
 
 export default function Trending() {
 
-    var axios = require("axios").default;
+  const [trendingData, setTrendingData] = useState([]);
 
-    const [trendingData, setTrendingData] = useState([]);
+  useEffect(()=>{
+    getTrending();
+  }, []);
 
-  useEffect(() => {
-    var promise = new Promise((resolve, reject) => {
-      axios
-        .get(
-          "https://api.themoviedb.org/3/trending/movies/day?api_key=2152a5d77cf288227bc1af054e424936&page=1"
-        )
-        .then((res) => {
-          resolve(res.data);
-          reject("not found");
-        });
-    });
-    promise.then((res) => {
-      setTrendingData(res);
-    });
-  }, [axios]);
-
-  console.log(trendingData);
+  const getTrending = async () => {
+    const api = await fetch("https://api.themoviedb.org/3/trending/movies/day?api_key=2152a5d77cf288227bc1af054e424936&page=1");
+    const data = await api.json();
+    setTrendingData(data.results);
+    console.log(data.results);
+  };
 
   return (
     <React.Fragment>
-    Trending
-    {}
+      <span>Trending</span>
+      <div className='trending'>
+        {
+          trendingData && trendingData.map((movie)=>(
+          <MovieCard key={movie.id} title={movie.name || movie.title} date={movie.first_air_date} vote={movie.vote_average} image={movie.poster_path}/>
+          ))
+        }
+      </div>
     </React.Fragment>
   );
 }
