66aed8f39639fa93006cd22abffd5842e65d2aaf
aryan jangid <aryan100jangid@gmail.com>
2022-04-17 22:07:22 +0530

    Add search functionality

--- src/Components/Trending/Search.js
+++ src/Components/Trending/Search.js
@@ -3,6 +3,7 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import SearchIcon from '@mui/icons-material/Search';
+import {useState} from 'react';
 
 const style = {
   position: 'absolute',
@@ -24,6 +25,23 @@ const boxStyle = {
 }
 
 export default function BasicTextFields() {
+
+  const [content, setContent] = useState([]);
+  const [query, setQuery] = useState("");
+
+
+  const search = async (searchText) => {
+     const api = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=2152a5d77cf288227bc1af054e424936&query=${searchText}&page=1`);
+     const data = await api.json();
+     setContent(data.results);
+     console.log(data.results);
+  };
+
+  const handleChange = event => {
+    setQuery(event.target.value);
+    console.log(event.target.value);
+  }
+
   return (
     <Box
       component="form"
@@ -31,8 +49,8 @@ export default function BasicTextFields() {
       noValidate
       autoComplete="off"
     >
-      <TextField sx={boxStyle} id="outlined-basic" label="Search for TV shows and Movies" variant="outlined" />
-      <Button variant="contained" endIcon={<SearchIcon fontSize="large"/>}>
+      <TextField sx={boxStyle} id="outlined-basic" label="Search Content..." variant="outlined" onChange={handleChange}/>
+      <Button variant="contained" endIcon={<SearchIcon fontSize="large"/>} onClick={(searchText) => search(`${searchText}`)}>
         Search
       </Button>
     </Box>
